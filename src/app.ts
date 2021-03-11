0a33b26ad47a388978c042ab653e9ff6b7fa70a4
pius.lee <pius.lee@samsung.com>
2021-03-11 18:34:18 +0900

    add rpc for location update && object detected

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,48 @@
 import * as MRE from "@microsoft/mixed-reality-extension-sdk";
-import { Guid } from "@microsoft/mixed-reality-extension-sdk";
+import { Asset, Guid } from "@microsoft/mixed-reality-extension-sdk";
+import axios from 'axios';
+
+const RPC = {
+    ObjectDetected: "object-detected",
+    LocationChanged: "location-changed"
+}
 
 export default class Ideas {
     private actorIndex: number;
     private assets: MRE.AssetContainer;
 
+    private detects: Array<ObjectInfo>;
+    private actors: Array<MRE.Actor>;
+
+    private postitAsset: Asset[];
+    private memos: Array<Memo>;
+
     constructor(private ctx: MRE.Context) {
+        this.detects = [];
+        this.actors = [];
+        this.memos = [];
         this.assets = new MRE.AssetContainer(this.ctx);
         this.ctx.onStarted(() => this.started());
     }
 
     private async started() {
-        this.ctx.rpc.on("location-update", this.locationUpdated.bind(this));
+        console.log("start 1");
+        this.postitAsset = await this.assets.loadGltf('postit.glb', 'box');
+        console.log("start 2");
+        this.ctx.rpc.on(RPC.ObjectDetected, this.onObjectDetected.bind(this));
+        this.ctx.rpc.on(RPC.LocationChanged, this.onLocationChanged.bind(this));
     }
 
-    private async createNote(txt: string, offset?: {x: number, y: number, z: number}, rotation? : {x: number, y: number, z: number}) {
+    private createNote(txt: string, offset?: {x: number, y: number, z: number}) {
         console.log("=============================== createNote start");
         this.actorIndex++;
         offset = (typeof offset === 'undefined') ? {x: 0, y: 0, z: 0} : offset;
-        const gltf = await this.assets.loadGltf('postit.glb', 'box');
         const note = MRE.Actor.CreateFromPrefab(this.ctx, {
-            firstPrefabFrom: gltf,
+            firstPrefabFrom: this.postitAsset,
             actor: {
                 name: 'note' + this.actorIndex,
                 transform: {
-                    app: { position: offset}
+                    app: { position: offset }
                 }
             }
         });
@@ -42,28 +60,83 @@ export default class Ideas {
                     contents: txt,
                     anchor: MRE.TextAnchorLocation.MiddleCenter,
                     color: { r: 255 / 255, g: 0 / 255, b: 0 / 255 },
-                    height: 0.1
+                    height: 0.01
                 }
             }
         });
         console.log("=============================== createNote end");
-
+        this.actors.push(note);
         return note;
     }
 
-    private locationUpdated(options: { userId: Guid; }, ...args: any[]) : void
+    private onObjectDetected(options: { userId: Guid; }, ...args: any[]) : void
     {
         console.log("=============================== 1");
         console.log(options);
         console.log(args);
 
-        const position = {
-            x: args[1],
-            y: args[2],
-            z: args[3]
-        };
+        const objectType = args[0];
+        const x = args[1];
+        const y = args[2];
+        const z = args[3];
+        const position = x instanceof Number && y instanceof Number && z instanceof Number ?
+            { x:0, y:0, z:0 } : {x, y, z};
 
-        this.createNote(`${args[0]}`, position);
+        this.detects.push({type: objectType, position});
+        this.syncObjects();
         console.log("=============================== 2");
     }
+
+    private onLocationChanged(options: { userId: Guid; }, ...args: any[]) : void
+    {
+        const locationId = args[0];
+        axios.get(`https://dev.arp.tizenservice.xyz/api/v1/object/locations/${locationId}`).
+        then(resp => {
+            this.memos = resp.data as Array<Memo>;
+            for (let i = 0; i < this.actors.length; i++)
+            {
+                this.actors[i].destroy();
+            }
+            this.syncObjects();
+        });
+    }
+
+    private syncObjects()
+    {
+        console.log("syncOjbects start ..............");
+        if (this.memos.length < 1)
+        {
+            return;
+        }
+        console.log("detects = " + this.detects.length);
+        for (let i = 0; i < this.detects.length; i++) {
+            for (let m = 0; m < this.memos.length; m++) {
+                if (this.memos[m].linkedObjectType === this.detects[i].type)
+                {
+                    console.log(`${m} : ${this.memos[m].linkedObjectType}, ${this.memos[m].contents}`);
+                    this.memos[m].linkedObject = this.createNote(
+                        `[${this.memos[m].linkedObjectType}] ${this.memos[m].contents}`,
+                        this.detects[i].position);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+interface ObjectInfo {
+    type: string;
+    position: {x: number, y: number, z: number};
+}
+
+interface Memo {
+    type: string;
+    name: string;
+    permission: string;
+    locationId: string;
+    linkedObjectType: string;
+    contents: string;
+    id: string;
+    creatorId: string;
+    linkedObject?: MRE.Actor;
 }
